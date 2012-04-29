b8f4666ab0f91d550122f1dc55c883eef050cb90
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-04-29 16:32:54 +0200

    Added .html.markdown processor.

---
+++ lib/directory_template/processor/markdown.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+
+
+class DirectoryTemplate
+  class Processor
+
+    # The ERB Processor treats the file-content as ERB template.
+    Markdown = Processor.register(:markdown_to_html, '*.html.markdown', 'Markdown to HTML Template Processor') do |data|
+      Markdown.require 'kramdown'
+      data.content = Kramdown::Document.new(data.content).to_html
+      data.chomp_suffix!
+    end
+  end
+end
