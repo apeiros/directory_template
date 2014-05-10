011aad3c2b946ae8ec6231a6854a98836c771265
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2014-05-10 22:03:56 +0200

    Code cosmetics (avoid lines >120 columns).

--- lib/directory_template/processor/stop.rb
+++ lib/directory_template/processor/stop.rb
@@ -4,7 +4,12 @@ class DirectoryTemplate
   class Processor
 
     # The 
-    Stop = Processor.register(:stop, "*.stop", "Terminate processing queue", "After .stop, no processor will be run anymore") do |data|
+    Stop = Processor.register(
+      :stop,
+      "*.stop",
+      "Terminate processing queue",
+      "After .stop, no processor will be run anymore",
+    ) do |data|
       data.chomp_suffix!
       throw :stop_processing
     end
