1db58eb2980fa3964c63678c485ef93814723d10
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2014-05-10 22:15:19 +0200

    Fixed documentation of the Stop processor.

--- lib/directory_template/processor/stop.rb
+++ lib/directory_template/processor/stop.rb
@@ -2,7 +2,9 @@
 
 class DirectoryTemplate
   class Processor
-    # The 
+    # The stop processor removes the ".stop" file extension and then terminates the processing queue.
+    # This can be used to have suffixes before the stop which would be processed, but should not.
+    # E.g. template.html.haml.stop.erb, the .erb part will be processed. But the haml won't be converted to html.
     Stop = Processor.register(
       :stop,
       "*.stop",
